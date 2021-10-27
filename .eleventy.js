5f9ca9604f7c975c257f9735cb0e08c901d5b7bb
tbreuss <thomasbreuss@gmx.ch>
2021-10-27 17:08:19 +0200

    Set markdown-it break to false

--- .eleventy.js
+++ .eleventy.js
@@ -270,7 +270,7 @@ module.exports = function(eleventyConfig) {
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
     html: true,
-    breaks: true,
+    breaks: false,
     linkify: true
     }).use(markdownItAnchor, {
     permalink: markdownItAnchor.permalink.ariaHidden({
