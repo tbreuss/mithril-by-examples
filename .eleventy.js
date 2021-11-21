4381d292fb2a2b6f0bc44392e21a97ed07cfdc82
tbreuss <thomasbreuss@gmx.ch>
2021-11-21 18:31:41 +0100

    Some fixes

--- .eleventy.js
+++ .eleventy.js
@@ -694,7 +694,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('docs/404.html');
+        const content_404 = fs.readFileSync('_site/404.html');
 
         browserSync.addMiddleware('*', (req, res) => {
           // Provides the 404 content without redirect.
@@ -746,7 +746,7 @@ module.exports = function(eleventyConfig) {
       input: '.',
       includes: '_includes',
       data: '_data',
-      output: 'docs'
+      output: '_site'
     }
   };
 };
