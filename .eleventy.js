016d0cb0197124beefbad7f953bcf44e0d94fbb8
tbreuss <thomasbreuss@gmx.ch>
2021-11-28 11:36:54 +0100

    Include local mithril.js, some cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -671,6 +671,7 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('.htaccess');
   eleventyConfig.addPassthroughCopy('robots.txt');
   eleventyConfig.addPassthroughCopy('CNAME');
